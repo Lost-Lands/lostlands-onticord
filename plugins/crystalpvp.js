237af2b6596cd1682e31708b8c1dd98a574e6c26
CM8DoubleCheck <68802586+CM8DoubleCheck@users.noreply.github.com>
2020-10-15 04:10:40 -0500

    Log crystal pvp deaths

--- plugins/crystalpvp.js
+++ plugins/crystalpvp.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 var mysql = require('mysql');
+const https = require('https');
+
 
 var pluginFolder = path.join(__dirname, "CrystalPVP");
 var configFile = path.join(pluginFolder, "config.json");
@@ -123,8 +125,57 @@ function announce(msg, client) {
     }
 }
 
-function logDeath(killer, victim, weapon) {
-    console.log(`${killer} killed ${victim} using ${weapon}`)
+function getUUID(username, callback) {
+    https.get('https://api.mojang.com/users/profiles/minecraft/'+username, (resp) => {
+        let data = '';
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            callback(null, JSON.parse(data).id);
+            
+        });
+    }).on("error", (err) => {
+        callback(err);
+    });
+}
+
+function uuidDashes(uuid) {
+    return uuid.slice(0,8)+"-"+uuid.slice(8,12)+"-"+uuid.slice(12,16)+"-"+uuid.slice(16,20)+"-"+uuid.slice(20,32)
+}
+function logDeath(killer, victim, weapon, database) {
+    var timestamp = Date.now();
+   log(`${killer} killed ${victim} using ${weapon}`)
+
+    getUUID(killer, function(err, uuid) {
+        if (err) {
+            console.err(err);
+        } else {
+            var killer_uuid = uuidDashes(uuid)
+            getUUID(victim, function(err, uuid) {
+                if (err) {
+                    console.err(err);
+                } else {
+                    var victim_uuid = uuidDashes(uuid)
+
+                    //log death
+                    console.log(killer, killer_uuid);
+                    console.log(victim, victim_uuid);                    
+                    database.query(`INSERT INTO cpvp_kills (killer_uuid, victim_uuid, weapon, timestamp) VALUES ("${killer_uuid}", "${victim_uuid}", "${weapon}", "${timestamp}")`, function(err, result) {
+                        if (err) {
+                            console.error(err);
+                        } else {
+                            log(`Logged kill for ${killer} on ${victim} using ${weapon} at ${timestamp}`);
+                        }
+                    })
+                    
+                }
+            })
+        }
+    })
+
 }
 
 
@@ -136,11 +187,18 @@ module.exports = (onticord) => {
             //config loaded
 
             var connection = mysql.createConnection(config.mysql);
-            connection.query("CREATE TABLE IF NOT EXISTS `cpvp_teams` (`ID` INT(6) NOT NULL, `uuid` VARCHAR(32) NOT NULL, `team_name` TEXT NOT NULL, `team_rank` TEXT NOT NULL, PRIMARY KEY (`ID`)) ENGINE = InnoDB;", function(err, result) {
+            connection.query("CREATE TABLE IF NOT EXISTS `cpvp_teams` (`ID` INT(6) AUTO_INCREMENT NOT NULL, `uuid` VARCHAR(36) NOT NULL, `team_name` TEXT NOT NULL, `team_rank` TEXT NOT NULL, PRIMARY KEY (`ID`)) ENGINE = InnoDB;", function(err, result) {
                 if (err) {
                     console.error(err);
                 } else {
                     log("Successfully ran query for MySQL cpvp_teams database");
+                }
+            });
+            connection.query("CREATE TABLE IF NOT EXISTS `cpvp_kills` (`ID` INT(6) AUTO_INCREMENT NOT NULL, `killer_uuid` VARCHAR(36) NOT NULL, `victim_uuid` VARCHAR(36) NOT NULL, `weapon` TEXT NOT NULL, `timestamp` bigint(16) NOT NULL, PRIMARY KEY (`ID`)) ENGINE = InnoDB;", function(err, result) {
+                if (err) {
+                    console.error(err);
+                } else {
+                    log("Successfully ran query for MySQL cpvp_kills database");
                 }
             });
 
@@ -171,7 +229,10 @@ module.exports = (onticord) => {
                                 killer = deathMessage[1].text.slice(0, -8);
                             }
                             if (config.plan_support == true) {
-                                logDeath(victim, killer, weapon);
+                                if (client.username == victim) { //prevents logging the kill n number of times where n is the total players connected
+                                    console.log(killer_uuid, victim_uuid);
+                                    logDeath(killer, victim, weapon, connection);
+                                }
                             }
                         }
                     }
